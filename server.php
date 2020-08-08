8b6e84c8e6dee3962e398da7c14c2d21777397cd
Deepesh Gupta <deepesh.gupta96@gmail.com>
2020-08-08 02:32:33 -0400

    Add files via upload

--- server.php
+++ server.php
@@ -36,7 +36,20 @@ if ($user) {
 	if ($email['email'] === $email){array_push($errors, "email already exists with username")}
 }
 
+//Register the user if no error
 
+if(count($errors) ==0){
+
+	$password = md5($password_1); //to encrypt pssword
+	$query = "INSERT INTO user (username, email, password) VALUES ($username, $email, $password)";
+
+	mysqli_query($db, $query);
+	$_SESSION['username'] = $username;
+	$_SESSION['success'] = "YOu are now logged in";
+
+	header('Location index.php')
+
+}
 
 
 
