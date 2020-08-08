b542ce7d44d74e9652e2172778f34be9c1663ad7
Deepesh Gupta <deepesh.gupta96@gmail.com>
2020-08-08 13:20:52 -0400

    Add files via upload

--- server.php
+++ server.php
@@ -6,12 +6,12 @@ session_start();
 
 $username = "";
 $email = "";
-
 $errors = array();
 
 //connect to db
 
 $db = mysqli_connect('localhost', 'root','','practice') or die("could not connect to database");
+
 //Register user
 
 $username = mysqli_real_escape_string($db, $_POST['username']);
@@ -20,10 +20,10 @@ $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
 $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
 
 //forms validation
-if (empty(username)) {array_push($errors, "Username is required")};
-if (empty(email)) {array_push($errors,"Email is required")};
-if (empty(password_1)) {array_push($errors, "Password is required")};
-if (password_1 != password_2) {echo "password 1 not same as password_2";}
+if (empty($username)) {array_push($errors, "Username is required");}
+if (empty($email)) {array_push($errors,"Email is required");}
+if (empty($password_1)) {array_push($errors, "Password is required");}
+if ($password_1 != $password_2) {array_push ($errors,"password 1 not same as password_2");}
 
 //Check db for existing with same username
 
@@ -32,47 +32,55 @@ $result = mysqli_query($db, $user_check_query);
 $user = mysqli_fetch_assoc($result);
 
 if ($user) {
-	if ($user['username']=== $username) { array_push($errors, "USername already exists");}
-	if ($email['email'] === $email){array_push($errors, "email already exists with username")}
+	if ($user['username']== $username) { array_push($errors, "Username already exists");}
+	if ($email['email'] == $email){array_push($errors, "email already exists with username");}
 }
 
 //Register the user if no error
 
-if(count($errors) ==0){
+if(count($errors) == 0){
 
-	$password = md5($password_1); //to encrypt pssword
-	$query = "INSERT INTO user (username, email, password) VALUES ($username, $email, $password)";
+	$password = md5($password_1); //to encrypt password
+	$query = "INSERT INTO user (username, email, password) VALUES ('$username', '$email', '$password')";
 
 	mysqli_query($db, $query);
 	$_SESSION['username'] = $username;
-	$_SESSION['success'] = "YOu are now logged in";
+	$_SESSION['success'] = "You are now logged in";
 
-	header('Location index.php')
-
+	header('location: index.php');
 }
 
 
+//Login users
 
+if(isset($_POST['login_user'])){
 
+	$username = mysqli_real_escape_string($db, $_POST['username']);
+	$password = mysqli_real_escape_string($db, $_POST['password_1']);
 
+	if(empty($username)){
+		array_push($errors, 'USername is required');
 
+	}
 
+	if (empty($password)) {
+		array_push($errors, 'Password is required');
+	}
 
+	if (count($errors) == 0) {
 
+		$password = md5(password);
+		$query = "SELECt * FROM user WHERE username ='$username' AND password = '$password' ";
+		$results = mysqli_query($db, $query);
+		if (mysqli_num_rows($results)) {
+			$_SESSION['username'] = $username;
+			$_SESSION['success'] = 'logged in successfully';
+			header('Location: index.php');
+		}else
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+		array_push($errors, 'Username or password is wrong please try again' );
+		
+	}
+}
 
 ?>
