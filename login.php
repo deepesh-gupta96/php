81bff041593cc9d6e19d6dbb53b7ba4e1ff5d2c3
Deepesh Gupta <deepesh.gupta96@gmail.com>
2020-08-08 01:55:39 -0400

    Login

---
+++ login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration</title>
+</head>
+<body>
+
+	<div class="container">
+		<div class="header">
+			
+			<h2>Register</h2>
+		</div>
+
+		<forma action= "registration.php" method = "post">
+
+			<div>
+				
+				<label for="username">Username</label>
+				<input type="text" name="username">
+
+			</div>
+
+			
+			<div>
+				
+				<label for="password">Password</label>
+				<input type="password" name="password">
+
+			</div>
+
+			
+			<button type="submit">Submit</button>
+
+			<p>Not a user? <a href="registration.php">Register here</a></p>
+
+
+
+		</form>
+
+
+	</div>
+
+
+</body>
+</html>
