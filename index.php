c0ab149e91d7bc9639ad15c19d1e7309850d3b4e
Deepesh Gupta <deepesh.gupta96@gmail.com>
2020-08-08 02:49:33 -0400

    Add files via upload

--- index.php
+++ index.php
@@ -11,14 +11,47 @@ if (isset($_GET['logout'])) {
 	unset($_SESSION['username']);
 	header('location: login.php');
 }
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home Page</title>
+</head>
+<body>
+
+<h1>Welcome to the page</h1>
+
+<?php 
+if(isset($_SESSION['sucsess'])); ?>
+
+<div>
+	
+	<h3>
+		
+		<<?php 
+
+		echo $_SESSION['success'];
+		unset($_SESSION['success']);
+
+		 ?>
+	</h3>
+</div>
+
+<?php endif ?>
+
+//if the user login in print information
+
+<?php
+
+if (isset($_SESSION['username']));?>
+
+<h3>Welcome <strong><?php echo $_SESSION['username'];?></strong></h3>
+
+<button><a href="index.php?logout = 1">Logout</a></button> 
+
+<?php endif ?>
 
 
-
-
-
-
-
-
-
-
- ?>
+</body>
+</html>
